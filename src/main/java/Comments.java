45e41cb6154757ee7e23c0f274f6ab3ab74cbd52
SergiiBal <sergey.balashchuk@gmail.com>
2023-06-01 22:13:36 +0300

    added comments

---
+++ src/main/java/Comments.java
@@ -0,0 +1,13 @@
+public class Comments {
+    //single line comment
+       /* multi
+          nmmj @author
+       line*/
+
+    /**
+     * @author Sergii
+     */
+    public
+    String name;
+
+}
