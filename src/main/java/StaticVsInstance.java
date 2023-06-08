de58616e3533fd851c9483b15b7ff6893bc8319b
Ostap Gonchar <ostap,gonchar@gmail.com>
2023-06-08 21:28:38 +0200

    static vs instance

---
+++ src/main/java/StaticVsInstance.java
@@ -0,0 +1,14 @@
+public class StaticVsInstance {
+    private static String staticValue = "Koza";
+    private String instanceValue = "Koza";
+
+    public static void main(String[] args) {
+        StaticVsInstance object1 = new StaticVsInstance();
+        object1.instanceValue = "Koza1";
+        StaticVsInstance object2 = new StaticVsInstance();
+        object2.instanceValue = "Koza2";
+        StaticVsInstance.staticValue = "A";
+        System.out.println(object1.instanceValue + ", " + StaticVsInstance.staticValue);
+        System.out.println(object2.instanceValue + ", " + StaticVsInstance.staticValue);
+    }
+}
