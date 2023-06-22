f79b2594dfb912146e914fba829e965d60b39f59
Ostap Gonchar <ostap,gonchar@gmail.com>
2023-06-22 21:00:30 +0200

    Override example

---
+++ src/main/java/lesson5/OverrideExample.java
@@ -0,0 +1,27 @@
+package lesson5;
+
+public class OverrideExample {
+
+    public static void main(String[] args) {
+        System.out.println("First Superclass");
+        new SuperClass().printMethod();
+        System.out.println("Now Subclass");
+        new SubClass().printMethod();
+    }
+}
+
+class SuperClass {
+    public void printMethod() {
+        System.out.println("Printed in Superclass.");
+    }
+}
+
+class SubClass extends SuperClass {
+    //inherits the methods from lesson4.SuperClass
+
+    @Override
+    public void printMethod() {
+        super.printMethod();
+        System.out.println("Also printed in SubClass");
+    }
+}
